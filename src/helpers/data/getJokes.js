5dc9adcd99092e72d22755c93e1c5a37b3a5b4f1
Chris LoJacono <clojacono92@gmail.com>
2020-11-07 10:16:18 -0600

    concole log removed

--- src/helpers/data/getJokes.js
+++ src/helpers/data/getJokes.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const getAJoke = (type) => new Promise((resolve, reject) => {
   axios.get(`https://official-joke-api.appspot.com/jokes/${type}/random`).then((response) => {
     resolve(response.data[0]);
-    console.warn(response.data[0]);
   }).catch((error) => reject(error));
 });
 
